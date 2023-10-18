9b9ada7cae080fe5bacf76fdb0c363cfaf3e24cb
박현수 <pm6567@naver.com>
2023-10-18 18:52:59 +0900

    regex말고 다른방법 생각해보기

--- src/main/kotlin/level7/10798.kt
+++ src/main/kotlin/level7/10798.kt
@@ -1,5 +1,20 @@
 package level7
 
-fun main () {
+fun main(){
+    var arr = Array(5){CharArray(15){ ' '}}
+    var sum = " "
+    for( i in 0 until 5){
+        var input = readLine()?: " "
+        for(j in input.indices){
+            arr[i][j]=input[j]
+        }
+    }
 
+    for(i in 0 until 15){
+        for( j in 0 until 5){
+            sum+=arr[j][i]
+        }
+    }
+    sum = sum.replace("\\s".toRegex(), "")
+    print(sum)
 }
